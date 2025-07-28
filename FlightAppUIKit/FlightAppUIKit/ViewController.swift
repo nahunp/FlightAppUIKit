ef47a953b6ccfd264303d9dd88481ef53d66968d
nahunp <nahun.perezv@gmail.com>
2025-07-28 15:35:18 -0400

    Initial Set Up

---
+++ FlightAppUIKit/FlightAppUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlightAppUIKit
+//
+//  Created by CincinnatiAi Dallas on 7/28/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
